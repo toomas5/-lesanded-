2514560d6e4df9bd5cfc4efb30e45ef5f0344748
toomas5 <115462520+toomas5@users.noreply.github.com>
2022-10-10 14:00:47 +0300

    Add files via upload

---
+++ kt01.py
@@ -0,0 +1,25 @@
+#toomas
+#10.10.22
+#yl2
+import turtle
+
+#akna seaded
+window = turtle.Screen()
+window.setup(400,400)
+
+#kujundi loomine
+t = turtle.Turtle()
+
+
+def kolm_nurk():
+    for x in range(3):
+        t.forward(50)
+        t.left(120)
+    t.left(60)
+
+kolm_nurk()
+kolm_nurk()
+kolm_nurk()
+kolm_nurk()
+kolm_nurk()
+kolm_nurk()
