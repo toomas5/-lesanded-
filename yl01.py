f5d5bf42a74a231638b91de12629c2c78711f294
toomas5 <115462520+toomas5@users.noreply.github.com>
2022-10-10 13:15:26 +0300

    Add files via upload

---
+++ yl01.py
@@ -0,0 +1,44 @@
+#toomas
+#10.10.22
+#ül1
+import turtle
+
+
+window = turtle.Screen()
+window.title('konn')
+window.setup(400,400)
+
+#kilpkonnade loomine ja värvid
+k1 = turtle.Turtle()
+k1.color ('green')
+k2 = turtle.Turtle()
+k2.color ('red')
+k3 = turtle.Turtle()
+k3.color ('orange')
+k4 = turtle.Turtle()
+k4.color ('blue')
+k5 = turtle.Turtle()
+k5.color ('blue')
+k6 = turtle.Turtle()
+k6.color ('green')
+k7 = turtle.Turtle()
+k7.color ('red')
+k8 = turtle.Turtle()
+k8.color ('orange')
+
+#kilkonnade liigutamine
+k1.forward(150)
+k2.left(45)
+k2.forward(150)
+k3.left(90)
+k3.forward(150)
+k4.left(135)
+k4.forward(150)
+k5.left(180)
+k5.forward(150)
+k6.left(225)
+k6.forward(150)
+k7.left(270)
+k7.forward(150)
+k8.left(315)
+k8.forward(150)
