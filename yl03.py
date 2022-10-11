9f8d65b512abac0ffc9127499d137ac2b5db049e
toomas5 <115462520+toomas5@users.noreply.github.com>
2022-10-11 11:40:25 +0300

    Add files via upload

---
+++ yl03.py
@@ -0,0 +1,16 @@
+#toomas
+#11.10.22
+#yl3
+
+
+alg = input("alg: ")
+lopp = input("lopp: ")
+
+email = input("email: ")
+print('@' in email)
+
+ropp = input("ropp: ")
+print(ropp.replace('kurat', '*****'))
+
+nimi = input("nimi: ")
+print(f"tere {nimi.capitalize()}!")
